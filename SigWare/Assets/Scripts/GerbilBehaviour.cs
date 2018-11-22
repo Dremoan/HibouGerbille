e1e6e64f1c2dc31f4aebe3960187dc17c486c762
Unknown <Robin.seignolles@hotmail.fr>
2018-11-22 17:51:58 +0100

    Ajout de barre de bruit

--- SigWare/Assets/Scripts/GerbilBehaviour.cs
+++ SigWare/Assets/Scripts/GerbilBehaviour.cs
@@ -1,39 +1,138 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HibouGerbille
 {
     public class GerbilBehaviour : MonoBehaviour
     {
 
-        public AnimationCurve curveSpeed;
-        [SerializeField] private float returnMoveDuration;
-        [SerializeField] private float speedMove = 10f;
+        [Range(0.1f, 1f)]
+        [SerializeField] private float waitBeforeNoiseBarDecrease = 1f;
+        [Range(0.0025f, 0.005f)]
+        [SerializeField] private float decreaseAmount;
+        [SerializeField] private Image noiseBar;
+
+        [Range(0.1f,0.5f)]
         [SerializeField] private float movingTime = 1f;
-        public bool isMoving = false;
+        [SerializeField] private float returnMoveDuration;
+        [SerializeField] private AnimationCurve curveSpeed;
+        [SerializeField] private AudioClip mouseSqueak;
+        [SerializeField] private AudioSource sourceAudio;
+        [SerializeField] private GameObject timelineDeath;
 
+        private bool isMoving = false;
+        private bool canMove = true;
+        private bool noiseBarDecrease = true;
+        private float percentMove;
+        private Vector3 StartPos;
+        private Vector3 EndPos;
+        public Vector3 distanceMoving;
+        [HideInInspector] public bool detecting;
 
+        private void Start()
+        {
+            StartPos = transform.position;
+            EndPos = transform.position;
+        }
         void Update()
         {
-            if (Input.GetMouseButtonDown(0) && !isMoving)
+            if (Input.GetMouseButtonDown(0) && !isMoving && canMove)
             {
-                StopAllCoroutines();
+                StartPos = EndPos;
+                StartCoroutine(BlockNoiseBarDecrease());
                 StartCoroutine(EnableMoving());
             }
 
-            if (isMoving)
+            if(isMoving)
             {
-                transform.Translate(transform.right * speedMove * Time.deltaTime);
+                JumpForward();
             }
+
+            if(noiseBarDecrease)
+            {
+                NoiseBarGestion();
+            }
+
+            GestionDetectionCircle();
+
         }
 
 
         IEnumerator EnableMoving()
         {
+            sourceAudio.PlayOneShot(mouseSqueak);
             isMoving = true;
+            StartPos = transform.position;
+            EndPos = StartPos + distanceMoving;
+            JumpForward();
             yield return new WaitForSeconds(movingTime);
             isMoving = false;
+            transform.position = EndPos;
+            returnMoveDuration = 0;
+        }
+
+        IEnumerator BlockNoiseBarDecrease()
+        {
+            noiseBar.fillAmount += .05f;
+            noiseBarDecrease = false;
+            yield return new WaitForSeconds(waitBeforeNoiseBarDecrease);
+            noiseBarDecrease = true;
+        }
+
+        void JumpForward()
+        {
+            returnMoveDuration += Time.deltaTime;
+            percentMove = returnMoveDuration / movingTime;
+            float timePercent = curveSpeed.Evaluate(percentMove);
+            transform.position = Vector3.Lerp(StartPos, EndPos, timePercent);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == "Howl" && isMoving)
+            {
+                canMove = false;
+                Debug.Log("QuéPasa");
+                timelineDeath.SetActive(true);
+            }
+        }
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.tag == "Howl" && isMoving)
+            {
+                canMove = false;
+                Debug.Log("QuéPasa");
+                timelineDeath.SetActive(true);
+            }
+        }
+
+        void NoiseBarGestion()
+        {
+            noiseBar.fillAmount -= decreaseAmount;
+            if(noiseBar.fillAmount < .2f)
+            {
+                noiseBar.fillAmount = .2f;
+            }
+        }
+
+        void GestionDetectionCircle()
+        {
+            if (noiseBar.fillAmount > 0.6f)
+            {
+                detecting = true;
+            }
+            else
+            {
+                detecting = false;
+            }
+
+            //if (detectionHowlCircle.fillAmount > 0.95f)
+            //{
+            //    detectionHowlCircle.fillAmount = 1f;
+
+            //}
         }
     }
 }
--- SigWare/Assets/Scripts/HowlBehavior.cs
+++ SigWare/Assets/Scripts/HowlBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HibouGerbille
 {
@@ -12,7 +13,15 @@ namespace HibouGerbille
         [SerializeField] private AudioSource audioSource;
         [SerializeField] private Animator animHowl;
         [SerializeField] private bool launchMusic = true;
+        [SerializeField] private bool increaseVolume = false;
+        [SerializeField] private bool decreaseVolume = false;
 
+        [SerializeField] private GameObject timelineDeath;
+        [SerializeField] private GerbilBehaviour gerbilScript;
+        [SerializeField] private Image detectionHowlCircle;
+        [SerializeField] private GameObject exclamationFolder;
+        [SerializeField] private Animator detectionAnim;
+        private bool detected;
         private int countIdle = 0;
         private int randomClipItem;
         private float randomWait;
@@ -27,6 +36,18 @@ namespace HibouGerbille
                 StopAllCoroutines();
                 StartCoroutine(TurnBackCoroutine());
             }
+
+            if(increaseVolume)
+            {
+                VolumeUp();
+            }
+
+            if(decreaseVolume)
+            {
+                VolumeDown();
+            }
+
+            FillingDetectionCircle();
         }
 
         public void WaitBeforeChangeIdle()
@@ -74,12 +95,12 @@ namespace HibouGerbille
         {
             launchMusic = false;
             randomClipItem = Random.Range(0, audioTab.Length);
-            audioSource.PlayOneShot(audioTab[randomClipItem].audioClip, 1);
+            audioSource.PlayOneShot(audioTab[randomClipItem].audioClip);
+            yield return new WaitForSeconds(1f);
+            increaseVolume = true;
             yield return new WaitForSeconds(audioTab[randomClipItem].clipDuration);
-            audioSource.Stop();
-            animHowl.SetBool("Return", true);
-            yield return new WaitForSeconds(Random.Range(0.5f, 2.5f));
-            animHowl.SetBool("Turning", true);
+            decreaseVolume = true;
+            StartCoroutine(IdleDefaultBeforeTurning());
         }
 
         IEnumerator IdleTurnToDefault()
@@ -88,6 +109,65 @@ namespace HibouGerbille
             yield return new WaitForSeconds(Random.Range(3, 5));
             animHowl.SetBool("Turning", false);
             launchMusic = true;
+        }
+
+        IEnumerator IdleDefaultBeforeTurning()
+        {
+            animHowl.SetBool("Return", true);
+            float randomTurnWait = Random.Range(0.25f, 1.5f);
+            Debug.Log(randomTurnWait);
+            yield return new WaitForSeconds(randomTurnWait);
+            animHowl.SetBool("Turning", true);
+            detected = false;
+        }
+
+        void VolumeUp()
+        {
+            audioSource.volume += 0.01f;
+
+            if(audioSource.volume == 1f)
+            {
+                increaseVolume = false;
+                audioSource.volume = 1f;
+            }
+
+        }
+
+        void VolumeDown()
+        {
+            audioSource.volume -= 0.01f;
+
+            if (audioSource.volume == 0f)
+            {
+                decreaseVolume = false;
+                audioSource.volume = 0f;
+            }
+        }
+
+        void FillingDetectionCircle()
+        {
+            if(gerbilScript.detecting == true && !detected)
+            {
+                detectionHowlCircle.fillAmount += 0.01f;
+                exclamationFolder.SetActive(true);
+
+            }
+            
+            if(gerbilScript.detecting == false)
+            {
+                detectionHowlCircle.fillAmount -= 0.01f;
+                exclamationFolder.SetActive(false);
+
+            }
+
+            if (detectionHowlCircle.fillAmount > 0.95f && !detected)
+            {
+                detectionHowlCircle.fillAmount = 0f;
+                detected = true;
+                detectionAnim.SetTrigger("Detected");
+                StopAllCoroutines();
+                StartCoroutine(IdleDefaultBeforeTurning());
+            }
         }
     }
 
